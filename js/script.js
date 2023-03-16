811e8ef0ff6354be123c97f8dc5e3457b83a16d3
Sofia Franco <sofiaariadnafranco@gmail.com>
2023-03-15 21:23:02 -0300

    adding sidebars, modifying their styles

--- js/script.js
+++ js/script.js
@@ -2,7 +2,7 @@ const $=(selector)=>document.querySelector(selector)
 
 //Imagen que ingresará el usuario//
 
-const url = prompt (`Ingrese su imagen`)
+/*const url = prompt (`Ingrese su imagen`)
 
-$("#image").src = url
+$("#image").src = url*/
 
